21dc2bb51f9c3897ba319fc5c130ba7b366ccdc1
id2k1149 <id2k1149@gmail.com>
2022-12-21 11:23:12 -0500

    add Pokemon model

--- PokemonJSON/Pokemon.swift
+++ PokemonJSON/Pokemon.swift
@@ -6,3 +6,32 @@
 //
 
 import Foundation
+
+struct PokemonApp {
+    let results: [Pokemon]
+}
+
+struct Pokemon {
+    let name: String
+    let url: String
+}
+
+struct PokemonURL {
+    let sprites: Sprites
+}
+
+struct Sprites {
+    let other: Other
+}
+
+struct Other {
+    let home: Home
+}
+
+struct Home {
+    let front: Front
+}
+
+struct Front {
+    let front_default: String
+}
